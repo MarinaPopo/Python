18762da8dc1a11ceb37188a907d68674f60b962f
MarinaPopo <c.ki.marina@gmail.com>
2023-04-25 10:32:34 +0500

    dz

--- main.py
+++ main.py
@@ -6748,14 +6748,14 @@ import sqlite3
     # """)
 
 
-with sqlite3.connect("db_4.db") as con:
-    cur = con.cursor()
-    cur.execute("""
-    SELECT *
-    FROM Ware
-    ORDER BY Price DESC
-    LIMIT 2, 5;
-    """)
+# with sqlite3.connect("db_4.db") as con:
+#     cur = con.cursor()
+#     cur.execute("""
+#     SELECT *
+#     FROM Ware
+#     ORDER BY Price DESC
+#     LIMIT 2, 5;
+#     """)
 
     # res = cur.fetchall()
     # print(res)
@@ -6766,3 +6766,62 @@ with sqlite3.connect("db_4.db") as con:
     # print(res)
     # res2 = cur.fetchmany(2)
     # print(res2)
+
+import sqlite3
+
+# cars = [
+#     ('BMW', 54000),
+#     ('Chevrolet', 46000),
+#     ('Daewoo', 38000),
+#     ('Citroen', 29000),
+#     ('Honda', 33000)
+# ]
+#
+# with sqlite3.connect('cars.db') as con:
+#     cur = con.cursor()
+#     cur.execute("""
+#     CREATE TABLE IF NOT EXISTS cars(
+#         car_id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         model TEXT,
+#         price INTEGER
+#     )""")
+#
+#     cur.executescript("""
+#         DELETE FROM cars WHERE model LIKE 'B%';
+#         UPDATE cars SET price = price + 100
+#     """)
+
+    # cur.execute("UPDATE cars SET price = :Price WHERE model LIKE 'B%'", {'Price': 0})
+
+    # cur.executemany("INSERT INTO cars VALUES(NULL, ?, ?)", cars)
+
+    # for car in cars:
+    #     cur.execute("INSERT INTO cars VALUES(NULL, ?, ?)", car)
+
+    # cur.execute("INSERT INTO cars VALUES(1, 'Renault', 22000)")
+    # cur.execute("INSERT INTO cars VALUES(2, 'Volvo', 29000)")
+    # cur.execute("INSERT INTO cars VALUES(3, 'Mercedes', 57000)")
+    # cur.execute("INSERT INTO cars VALUES(4, 'Bentley', 35000)")
+    # cur.execute("INSERT INTO cars VALUES(5, 'Audi', 52000)")
+
+
+try:
+    con = sqlite3.connect('cars.db')
+    cur = con.cursor()
+    cur.executescript("""CREATE TABLE IF NOT EXISTS cars(
+        car_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        model TEXT,
+        price INTEGER
+    );
+    BEGIN;
+    INSERT INTO cars VALUES(NULL, 'Renault', 22000);
+    UPDATE cars2 SET price = price + 100;
+    """)
+    con.commit()
+except sqlite3.Error as e:
+    if con:
+        con.rollback()
+    print("Ошибка выполнения запроса", e)
+finally:
+    if con:
+        con.close
